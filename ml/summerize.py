e48e5cd1dd90d239984056980aaa4b2e95877346
fabf28 <ifavour228@gmail.com>
2025-10-05 07:42:24 -0600

    new csv data and gemini code

--- ml/summerize.py
+++ ml/summerize.py
@@ -1,35 +1,31 @@
-from openai import OpenAI
 import os
+import google.generativeai as genai
 
-AZURE_OPENAI_KEY        = os.getenv("AZURE_OPENAI_KEY")
-AZURE_OPENAI_ENDPOINT   = os.getenv("AZURE_OPENAI_ENDPOINT")
-AZURE_OPENAI_DEPLOYMENT = os.getenv("AZURE_OPENAI_DEPLOYMENT")
-AZURE_API_VERSION       = os.getenv("AZURE_API_VERSION")
+#import env variables
+GEMINI_KEY = os.getenv("GEMINI_KEY")
 
-client = OpenAI(api_key=AZURE_OPENAI_KEY)
+# Configure your key
+genai.configure(api_key=GEMINI_KEY)
+
+# Create a model instance
+model = genai.GenerativeModel("gemini-1.5-flash")  # or "gemini-1.5-pro"
+
 
 def responseText():
-    setup = "You are a helpful assistant."
     prompt = "Write a haiku about data science."
 
-    response = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[
-            {"role": "system", "content": setup},
-            {"role": "user", "content": prompt}
-        ]
-    )
-    return response
+    # Send a prompt
+    response = model.generate_content(prompt)
+    print(response.text)
+    return response.text
 
 def responseChart():
-    setup = "You are a helpful assistant."
     prompt = "Write a haiku about data science."
 
-    response = client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[
-            {"role": "system", "content": setup},
-            {"role": "user", "content": prompt}
-        ]
-    )
-    return response
+    # Send a prompt
+    response = model.generate_content(prompt)
+    print(response.text)
+    return response.text
+
+
+responseChart()
