1c5938771da383f07abf28bb27a978f2a050632e
sleepyprogrammerinc <salmaaboulnaga@gmail.com>
2025-10-05 09:51:07 -0600

    frontend changes for aesthetics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,7 @@ function Layout() {
   return (
     <>
       <Navbar />
-      <main className="container">
+      <main className="container" style={{ paddingTop: 80 }}>
         <Outlet />
       </main>
     </>
